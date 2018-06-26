2475ec186411c471832fb5fabb8b36e441ad12d2
Victor <victorasantekyei@yahoo.co.uk>
2018-06-26 14:04:15 +0200

    validate form

--- packages/web/src/modules/register/ui/RegisterView.tsx
+++ packages/web/src/modules/register/ui/RegisterView.tsx
@@ -1,25 +1,36 @@
 import { Button, Checkbox, Form, Icon, Input } from 'antd';
 import * as React from 'react';
 import { withFormik, FormikErrors, FormikProps } from 'formik';
+import * as yup from 'yup';
 
 const FormItem = Form.Item;
 
 interface FormValues {
   email: string;
-  password: string
+  password: string;
 }
 
 interface Props {
-  submit: (values: FormValues) => Promise<FormikErrors<FormValues> | null >
+  submit: (values: FormValues) => Promise<FormikErrors<FormValues> | null>;
 }
 
 export class C extends React.PureComponent<FormikProps<FormValues> & Props> {
   render() {
-    const { values, handleChange, handleBlur, handleSubmit } = this.props
+    const {
+      values,
+      handleChange,
+      handleBlur,
+      handleSubmit,
+      touched,
+      errors
+    } = this.props;
     return (
       <form style={{ display: 'flex' }} onSubmit={handleSubmit}>
         <div style={{ width: 400, margin: 'auto' }}>
-          <FormItem>
+          <FormItem
+            help={touched.email && errors.email ? errors.email : ''}
+            validateStatus={touched.email && errors.email ? 'error' : undefined}
+          >
             <Input
               name="email"
               prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />}
@@ -27,9 +38,15 @@ export class C extends React.PureComponent<FormikProps<FormValues> & Props> {
               value={values.email}
               onChange={handleChange}
               onBlur={handleBlur}
-            />,
+            />
           </FormItem>
-          <FormItem>
+          <FormItem
+            help={touched.password && errors.password ? errors.password : ''}
+            // tslint:disable-next-line:jsx-no-multiline-js
+            validateStatus={
+              touched.password && errors.password ? 'error' : undefined
+            }
+          >
             <Input
               name="password"
               prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />}
@@ -38,7 +55,7 @@ export class C extends React.PureComponent<FormikProps<FormValues> & Props> {
               value={values.password}
               onChange={handleChange}
               onBlur={handleBlur}
-            />,
+            />
           </FormItem>
           <FormItem>
             <Checkbox>Remember me</Checkbox>,
@@ -58,18 +75,39 @@ export class C extends React.PureComponent<FormikProps<FormValues> & Props> {
           <FormItem>
             Or <a href="">log in now!</a>
           </FormItem>
-        </ div>
+        </div>
       </form>
     );
   }
 }
 
+const emailNotLongEnough = 'email must be at least 3 characters';
+const passwordNotLongEnough = 'password must be at least 3 characters';
+const invalidEmail = 'email must be a valid email';
+
+const validationSchema = yup.object().shape({
+  email: yup
+    .string()
+    .min(3, emailNotLongEnough)
+    .max(255)
+    .email(invalidEmail)
+    .required(),
+  password: yup
+    .string()
+    .min(3, passwordNotLongEnough)
+    .max(255)
+    .required()
+});
+
 export const RegisterView = withFormik<Props, FormValues>({
-  mapPropsToValues: () => ({ email: '', password: ''}),
-  handleSubmit: async (values, {props, setErrors, setSubmitting}) => {
-    const errors = await props.submit(values)
+  validationSchema,
+  // validateOnChange: false,
+  // validateOnBlur: false,
+  mapPropsToValues: () => ({ email: '', password: '' }),
+  handleSubmit: async (values, { props, setErrors, setSubmitting }) => {
+    const errors = await props.submit(values);
     if (errors) {
-      setErrors(errors)
+      setErrors(errors);
     }
   }
 })(C);
